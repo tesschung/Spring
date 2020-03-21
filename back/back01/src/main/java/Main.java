9e8413a8c04ebc699a34649597519d477a147dfa
tesschung <geobera0910@naver.com>
2020-03-21 21:37:17 +0900

    Session && Cookie

---
+++ back/back01/src/main/java/Main.java
@@ -0,0 +1,9 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+        Student student1 = new Student();
+        student1.setName("정승원");
+        System.out.println(student1.getName());
+    }
+}
---
+++ back/back01/src/main/java/Student.java
@@ -0,0 +1,13 @@
+public class Student {
+
+    String name = null;
+
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+}
